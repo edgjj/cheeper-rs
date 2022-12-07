8a471131477821b76355e0c02a960a8de20fbe5a
edgjj <tstd@tstd.tech>
2022-12-07 14:02:51 +0500

    Args & Cargo: Add build alias for local OS server. Parse args to get engine (OS) URL.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use actix_web::{App, HttpServer, web};
 // use es_cheaper::dto::*;
 use es_cheaper::services::*;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args{
+    #[arg(short = 'e', long = "engine")]
+    engine_url: String
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
-    HttpServer::new(|| { 
+    let args = Args::parse();
+
+    HttpServer::new(move || { 
         App::new()
-            .app_data(web::Data::new(ServerState::new("http://localhost:8080")))
+            .app_data(web::Data::new(ServerState::new(args.engine_url.as_str())))
             .service(messaging::index_messages)
             .service(messaging::send_message)
     })
